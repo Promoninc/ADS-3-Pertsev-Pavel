a9584fb302a2cd2230a85e6353fb272432c528d8
Promoninc <promoninc@gmail.com>
2024-05-03 02:23:04 +0500

    MyHashNode class is added

---
+++ src/MyHashTable.java
@@ -0,0 +1,16 @@
+public class MyHashTable<K, V> {
+    private class MyHashNode<K, V>{
+        private K key;
+        private V value;
+        private MyHashNode<K, V> next;
+        MyHashNode(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+        @Override
+        public String toString(){
+            return "{" + key + " " + value + "}";
+        }
+    }
+
+}
