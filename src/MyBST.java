a8caa2b7c622fcdc8e6a598fcfd485cff64855b8
Promoninc <promoninc@gmail.com>
2024-05-08 16:49:34 +0500

    Created MyTestingClass, added MyBST class

---
+++ src/MyBST.java
@@ -0,0 +1,2 @@
+public class MyBST {
+}
---
+++ src/MyTestingClass.java
@@ -0,0 +1,21 @@
+public class MyTestingClass {
+    private String name;
+    private String surname;
+    private int age;
+    MyTestingClass(String name, String surname, int age){
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+    public int hashCode(){
+        int hash = 0;
+        for(int i = 0; i < name.length(); i++){
+            hash = name.toCharArray()[i] + (hash * 31);
+        }
+        for(int i = 0; i < surname.length(); i++){
+            hash = surname.toCharArray()[i] + (hash * 31);
+        }
+        hash += age;
+        return hash;
+    }
+}
